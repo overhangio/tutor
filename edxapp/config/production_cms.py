dcce76cabec3bcab49654dc52e7078dff4d60cd9
Régis Behmo <regis@behmo.com>
2018-03-16 17:12:26 +0100

    Override default email sender addresses
    This closes #13.

--- edxapp/config/production_cms.py
+++ edxapp/config/production_cms.py
@@ -3,3 +3,7 @@ from .production_common import *
 ALLOWED_HOSTS = [
     ENV_TOKENS.get('CMS_BASE'),
 ]
+
+DEFAULT_FROM_EMAIL = 'registration@' + ENV_TOKENS['LMS_BASE']
+DEFAULT_FEEDBACK_EMAIL = 'feedback@' + ENV_TOKENS['LMS_BASE']
+SERVER_EMAIL = 'devops@' + ENV_TOKENS['LMS_BASE']
