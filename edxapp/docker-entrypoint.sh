1f7e68c662f8fbf459c4ed75071772dbeb119f3e
Régis Behmo <regis@behmo.com>
2018-04-09 19:16:58 +0200

    Working devstack
    This allows the user to run their own devstack inside the containers.
    Yay!
    
    Also, we handle file permissions cleanly: in docker-entrypoint.sh we
    chmod the data and edx-platform files to the same UID of the user on the
    host machine. No more permission headaches!

---
+++ edxapp/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+export DJANGO_SETTINGS_MODULE=$SERVICE_VARIANT.envs.$SETTINGS
+USERID=${USERID:=1000}
+
+## Configure user with a different USERID if requested.
+if [ "$USERID" -ne 1000 ]
+    then
+        echo "creating new user 'openedx' with UID $USERID"
+        useradd -m openedx -u $USERID
+        chown -R openedx /openedx
+
+        # Run CMD as different user
+        exec chroot --userspec="$USERID" --skip-chdir / "$@"
+else 
+        # Run CMD as root (business as usual)
+        exec "$@"
+fi
