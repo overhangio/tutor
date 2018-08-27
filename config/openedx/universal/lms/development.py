75d7ee688b54c12c71477db92a15fabcc1de2122
Régis Behmo <regis@behmo.com>
2018-08-27 23:19:16 +0200

    Fix profile image upload
    Media file upload, such as video files or profile images, were uploaded
    to /edx/var/edxapp/media because the MEDIA_ROOT setting is defined in
    common.py.
    
    Close #54.

--- config/openedx/universal/lms/development.py
+++ config/openedx/universal/lms/development.py
@@ -15,3 +15,8 @@ import os
 for folder in [LOG_DIR, MEDIA_ROOT, STATIC_ROOT_BASE]:
     if not os.path.exists(folder):
         os.makedirs(folder)
+
+# Fix media files paths
+VIDEO_IMAGE_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
+VIDEO_TRANSCRIPTS_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
+PROFILE_IMAGE_BACKEND['options']['location'] = os.path.join(MEDIA_ROOT, 'profile-images/')
--- config/openedx/universal/lms/production.py
+++ config/openedx/universal/lms/production.py
@@ -1,8 +1,9 @@
-import os
 from ..aws import *
 
+# Load module store settings from config files
 update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
 
+# Set uploaded media file path
 MEDIA_ROOT = "/openedx/data/uploads/"
 
 # Change syslog-based loggers which don't work inside docker containers
@@ -10,9 +11,15 @@ LOGGING['handlers']['local'] = LOGGING['handlers']['console'].copy()
 LOGGING['handlers']['tracking'] = LOGGING['handlers']['console'].copy()
 
 # Create folders if necessary
+import os
 for folder in [LOG_DIR, MEDIA_ROOT, STATIC_ROOT_BASE]:
     if not os.path.exists(folder):
         os.makedirs(folder)
+
+# Fix media files paths
+VIDEO_IMAGE_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
+VIDEO_TRANSCRIPTS_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
+PROFILE_IMAGE_BACKEND['options']['location'] = os.path.join(MEDIA_ROOT, 'profile-images/')
 
 ALLOWED_HOSTS = [
     ENV_TOKENS.get('LMS_BASE'),
