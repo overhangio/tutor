deeb475428fbda78522a591e5504921969a4e84c
Daniel <danniel@gmail.com>
2019-01-25 21:27:33 -0300

    Fix video srt & transcript upload
    Transcripts and video were being uploaded to /edx/var/edxapp/media.
    
    This is for issue #138.

--- deploy/templates/openedx/settings/lms/production.py
+++ deploy/templates/openedx/settings/lms/production.py
@@ -8,6 +8,10 @@ update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
 # Set uploaded media file path
 MEDIA_ROOT = "/openedx/data/uploads/"
 
+# Video settings
+VIDEO_IMAGE_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
+VIDEO_TRANSCRIPTS_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
+
 # Change syslog-based loggers which don't work inside docker containers
 LOGGING['handlers']['local'] = {'class': 'logging.NullHandler'}
 LOGGING['handlers']['tracking'] = {
