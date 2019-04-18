5eda14020e71e628a5280a52f1c311976e50430e
Silvio Tomatis <silviot@gmail.com>
2019-04-18 12:56:15 +0200

    Configure lms and cms to log to a file

--- tutor/templates/apps/openedx/settings/cms/common.py
+++ tutor/templates/apps/openedx/settings/cms/common.py
@@ -1,5 +1,7 @@
 """Settings shared between production and development
 """
+import os
+
 
 # Set uploaded media file path
 MEDIA_ROOT = "/openedx/data/uploads/"
@@ -9,9 +11,21 @@ VIDEO_IMAGE_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
 VIDEO_TRANSCRIPTS_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
 
 # Change syslog-based loggers which don't work inside docker containers
-LOGGING['handlers']['local'] = {'class': 'logging.NullHandler'}
+LOGGING_DEFAULT_LEVEL = os.environ.get('OPENEDX_LOGLEVEL', 'INFO')
+settings.LOGGING['loggers']['']['level'] = LOGGING_DEFAULT_LEVEL
+
+LOGGING_FILENAME = os.path.join(LOG_DIR,
+    '{}.log'.format(os.environ.get('SERVICE_VARIANT', 'other')))
+LOGGING['handlers']['local'] = {
+    'level': LOGGING_DEFAULT_LEVEL,
+    'class': 'logging.handlers.WatchedFileHandler',
+    'filename': LOGGING_FILENAME,
+    'formatter': 'standard',
+}
+
 LOGGING['handlers']['tracking'] = {
     'level': 'DEBUG',
-    'class': 'logging.StreamHandler',
+    'class': 'logging.handlers.WatchedFileHandler',
+    'filename': os.path.join(LOG_DIR, 'tracking.log'),
     'formatter': 'standard',
 }
