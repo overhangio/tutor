205fe720d2d23ee64843eca894cf4dd17d7df7b1
Silvio Tomatis <silviot@gmail.com>
2019-04-24 15:40:35 +0200

    Factor out common logic for lms/{production/development}.py

--- tutor/templates/apps/openedx/settings/lms/production.py
+++ tutor/templates/apps/openedx/settings/lms/production.py
@@ -1,28 +1,9 @@
 import os
 from lms.envs.production import *
 
-# Load module store settings from config files
-update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
 
-# Set uploaded media file path
-MEDIA_ROOT = "/openedx/data/uploads/"
-
-# Video settings
-VIDEO_IMAGE_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
-VIDEO_TRANSCRIPTS_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
-
-# Change syslog-based loggers which don't work inside docker containers
-LOGGING['handlers']['local'] = {'class': 'logging.NullHandler'}
-LOGGING['handlers']['tracking'] = {
-    'level': 'DEBUG',
-    'class': 'logging.StreamHandler',
-    'formatter': 'standard',
-}
-
-# Fix media files paths
-VIDEO_IMAGE_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
-VIDEO_TRANSCRIPTS_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
-PROFILE_IMAGE_BACKEND['options']['location'] = os.path.join(MEDIA_ROOT, 'profile-images/')
+# Execute the contents of common.py in this context
+execfile(os.path.join(os.path.dirname(__file__), 'common.py'), globals())
 
 ALLOWED_HOSTS = [
     ENV_TOKENS.get('LMS_BASE'),
@@ -49,22 +30,3 @@ PAYMENT_SUPPORT_EMAIL = ENV_TOKENS['CONTACT_EMAIL']
 BULK_EMAIL_DEFAULT_FROM_EMAIL = 'no-reply@' + ENV_TOKENS['LMS_BASE']
 API_ACCESS_MANAGER_EMAIL = ENV_TOKENS['CONTACT_EMAIL']
 API_ACCESS_FROM_EMAIL = ENV_TOKENS['CONTACT_EMAIL']
-
-ORA2_FILEUPLOAD_BACKEND = 'filesystem'
-ORA2_FILEUPLOAD_ROOT = '/openedx/data/ora2'
-ORA2_FILEUPLOAD_CACHE_NAME = 'ora2-storage'
-
-GRADES_DOWNLOAD = {
-    'STORAGE_TYPE': '',
-    'STORAGE_KWARGS': {
-        'base_url': "/media/grades/",
-        'location': os.path.join(MEDIA_ROOT, 'grades'),
-    }
-}
-
-LOCALE_PATHS.append('/openedx/locale')
-
-# Create folders if necessary
-for folder in [LOG_DIR, MEDIA_ROOT, STATIC_ROOT_BASE, ORA2_FILEUPLOAD_ROOT]:
-    if not os.path.exists(folder):
-        os.makedirs(folder)
