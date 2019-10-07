cb0e2c509db8a540a09b1c9ba75676d5a2b7f0d2
Régis Behmo <regis@behmo.com>
2019-10-07 16:37:01 +0200

    De-duplicate asset settings

---
+++ tutor/templates/build/openedx/settings/partials/assets.py
@@ -0,0 +1,17 @@
+"""
+Bare minimum settings for collecting production assets.
+"""
+from ..common import *
+from openedx.core.lib.derived import derive_settings
+
+COMPREHENSIVE_THEME_DIRS.append('/openedx/themes')
+STATIC_ROOT_BASE = '/openedx/staticfiles'
+
+SECRET_KEY = 'secret'
+XQUEUE_INTERFACE = {
+    'django_auth': None,
+    'url': None,
+}
+DATABASES = {
+    "default": {},
+}
