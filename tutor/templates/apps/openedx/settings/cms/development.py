11f73852763a8e74444b2c3d074dc707db4f3477
Régis Behmo <regis@behmo.com>
2019-10-18 11:36:07 +0200

    Fix cms development settings
    LMS settings were being incorrectly included in the cms dev settings.

--- tutor/templates/apps/openedx/settings/cms/development.py
+++ tutor/templates/apps/openedx/settings/cms/development.py
@@ -1,7 +1,7 @@
 import os
 from cms.envs.devstack import *
 
-{% include "apps/openedx/settings/partials/common/lms.py" %}
+{% include "apps/openedx/settings/partials/common/cms.py" %}
 
 # Setup correct webpack configuration file for development
 WEBPACK_CONFIG_PATH = "webpack.dev.config.js"
