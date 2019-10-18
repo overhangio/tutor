ed0cf8d99ffd41e9832560c057921135c3a25502
Régis Behmo <regis@behmo.com>
2019-10-18 18:01:32 +0200

    Fix incorrect lms settings patch

--- tutor/templates/apps/openedx/settings/lms/development.py
+++ tutor/templates/apps/openedx/settings/lms/development.py
@@ -1,7 +1,7 @@
 import os
 from lms.envs.devstack import *
 
-{% include "apps/openedx/settings/partials/common_cms.py" %}
+{% include "apps/openedx/settings/partials/common_lms.py" %}
 
 # Setup correct webpack configuration file for development
 WEBPACK_CONFIG_PATH = "webpack.dev.config.js"
