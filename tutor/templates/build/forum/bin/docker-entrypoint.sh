30e816d2323e4352b37f1bab27c7bd99e4b30dd0
Régis Behmo <regis@behmo.com>
2020-04-17 18:31:08 +0200

    Fix forum init job
    Now, when we run init for the forum, we go through the forum container
    entrypoint. At this stage, the elasticsearch:9200/content url returns a
    404 error, because, well, the init job has not run yet. So instead of
    checking for the /content url, we simply check that the elasticsearch
    container is up and running. Note that this might cause the initial
    forum container to crash right after start.

--- tutor/templates/build/forum/bin/docker-entrypoint.sh
+++ tutor/templates/build/forum/bin/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 export MONGOHQ_URL="mongodb://$MONGODB_AUTH$MONGODB_HOST:$MONGODB_PORT/cs_comments_service"
 
 echo "Waiting for mongodb/elasticsearch..."
-dockerize -wait tcp://$MONGODB_HOST:$MONGODB_PORT -wait $SEARCH_SERVER/content -wait-retry-interval 5s -timeout 600s
+dockerize -wait tcp://$MONGODB_HOST:$MONGODB_PORT -wait $SEARCH_SERVER -wait-retry-interval 5s -timeout 600s
 
 exec "$@"
