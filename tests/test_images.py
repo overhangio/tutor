5f81aae6164ecb113419d9af8e06eaf6b482df64
Régis Behmo <regis@behmo.com>
2020-07-21 09:28:21 +0200

    Fix `images.get_tag` unit test

--- tests/test_images.py
+++ tests/test_images.py
@@ -5,9 +5,8 @@ from tutor import images
 class ImagesTests(unittest.TestCase):
     def test_get_tag(self):
         config = {
-            "DOCKER_IMAGE_OPENEDX": "openedx",
-            "DOCKER_IMAGE_OPENEDX_DEV": "openedxdev",
-            "DOCKER_REGISTRY": "registry/",
+            "DOCKER_IMAGE_OPENEDX": "registry/openedx",
+            "DOCKER_IMAGE_OPENEDX_DEV": "registry/openedxdev",
         }
         self.assertEqual("registry/openedx", images.get_tag(config, "openedx"))
         self.assertEqual("registry/openedxdev", images.get_tag(config, "openedx-dev"))
