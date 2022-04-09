58851a2bbf1ff8c3110a84b941decfbd73cc29d6
Kyle McCormick <kyle@tcril.org>
2022-04-09 00:38:44 -0400

    feat!: remove `runserver` in favor of `start -d`
    Previously, Open edX developers were recommend to use:
    
        tutor dev runserver [lms|cms]
    
    to start the platform. This was because a typical use of
    `start` did not allocate a TTY or open STDIN, making it
    difficult/impossible for developers to interact with the
    container process and make use of dev techniques like
    breakpoints.
    
    Now, `start` will allocate a TTY and open STDIN for
    development containers, allowing users to run either:
    
        tutor dev start -d lms cms
        tutor dev start -d  # Starts all enabled services.
    
    By making it so that there's only one obvious way
    to start services, we hope that it is easier for
    Tutor dev users to get oriented.

--- tutor/commands/dev.py
+++ tutor/commands/dev.py
@@ -42,7 +42,7 @@ def dev(context: click.Context) -> None:
 
 
 @click.command(
-    help="Run a development server",
+    help="DEPRECATED - Use \"tutor dev start -d SERVICE\" instead.",
     context_settings={"ignore_unknown_options": True},
 )
 @click.argument("options", nargs=-1, required=False)
@@ -50,16 +50,7 @@ def dev(context: click.Context) -> None:
 @click.pass_context
 def runserver(context: click.Context, options: List[str], service: str) -> None:
     config = tutor_config.load(context.obj.root)
-    if service in ["lms", "cms"]:
-        port = 8000 if service == "lms" else 8001
-        host = config["LMS_HOST"] if service == "lms" else config["CMS_HOST"]
-        fmt.echo_info(
-            "The {} service will be available at http://{}:{}".format(
-                service, host, port
-            )
-        )
-    args = ["--service-ports", *options, service]
-    context.invoke(compose.run, args=args)
+    context.invoke(compose.start, "--detached", *options, service)
 
 
 dev.add_command(runserver)
