29eb3398a2105b2ea55b26a86e2acf843d7e24f5
Régis Behmo <regis@behmo.com>
2022-11-17 14:14:49 +0100

    feat: auto-complete `config save/printroot` arguments

--- tutor/commands/config.py
+++ tutor/commands/config.py
@@ -1,12 +1,14 @@
-from typing import List
+import json
+import typing as t
 
 import click
+import click.shell_completion
 
 from .. import config as tutor_config
 from .. import env, exceptions, fmt
 from .. import interactive as interactive_config
 from .. import serialize
-from ..types import Config
+from ..types import Config, ConfigValue
 from .context import Context
 
 
@@ -19,13 +21,82 @@ def config_command() -> None:
     pass
 
 
+class ConfigKeyParamType(click.ParamType):
+
+    name = "configkey"
+
+    def shell_complete(
+        self, ctx: click.Context, param: click.Parameter, incomplete: str
+    ) -> t.List[click.shell_completion.CompletionItem]:
+        return [
+            click.shell_completion.CompletionItem(key)
+            for key, _value in self._shell_complete_config_items(ctx, incomplete)
+        ]
+
+    @staticmethod
+    def _shell_complete_config_items(
+        ctx: click.Context, incomplete: str
+    ) -> t.List[t.Tuple[str, ConfigValue]]:
+        # Here we want to auto-complete the name of the config key. For that we need to
+        # figure out the list of enabled plugins, and for that we need the project root.
+        # The project root would ordinarily be stored in ctx.obj.root, but during
+        # auto-completion we don't have access to our custom Tutor context. So we resort
+        # to a dirty hack, which is to examine the grandparent context.
+        root = getattr(
+            getattr(getattr(ctx, "parent", None), "parent", None), "params", {}
+        ).get("root", "")
+        config = tutor_config.load_full(root)
+        return [
+            (key, value) for key, value in config.items() if key.startswith(incomplete)
+        ]
+
+
+class ConfigKeyValParamType(ConfigKeyParamType):
+    """
+    Parser for <KEY>=<YAML VALUE> command line arguments.
+    """
+
+    name = "configkeyval"
+
+    def convert(self, value: str, param: t.Any, ctx: t.Any) -> t.Tuple[str, t.Any]:
+        result = serialize.parse_key_value(value)
+        if result is None:
+            self.fail(f"'{value}' is not of the form 'key=value'.", param, ctx)
+        return result
+
+    def shell_complete(
+        self, ctx: click.Context, param: click.Parameter, incomplete: str
+    ) -> t.List[click.shell_completion.CompletionItem]:
+        """
+        Nice and friendly <KEY>=<VAL> auto-completion.
+        """
+        if "=" not in incomplete:
+            # Auto-complete with '<KEY>='. Note the single quotes which allow users to
+            # further auto-complete later.
+            return [
+                click.shell_completion.CompletionItem(f"'{key}='")
+                for key, value in self._shell_complete_config_items(ctx, incomplete)
+            ]
+        if incomplete.endswith("="):
+            # raise ValueError(f"incomplete: <{incomplete}>")
+            # Auto-complete with '<KEY>=<VALUE>'
+            return [
+                click.shell_completion.CompletionItem(f"{key}={json.dumps(value)}")
+                for key, value in self._shell_complete_config_items(
+                    ctx, incomplete[:-1]
+                )
+            ]
+        # Else, don't bother
+        return []
+
+
 @click.command(help="Create and save configuration interactively")
 @click.option("-i", "--interactive", is_flag=True, help="Run interactively")
 @click.option(
     "-s",
     "--set",
     "set_vars",
-    type=serialize.YamlParamType(),
+    type=ConfigKeyValParamType(),
     multiple=True,
     metavar="KEY=VAL",
     help="Set a configuration value (can be used multiple times)",
@@ -35,17 +106,18 @@ def config_command() -> None:
     "--unset",
     "unset_vars",
     multiple=True,
+    type=ConfigKeyParamType(),
     help="Remove a configuration value (can be used multiple times)",
 )
 @click.option(
-    "-e", "--env-only", "env_only", is_flag=True, help="Skip updating config.yaml"
+    "-e", "--env-only", "env_only", is_flag=True, help="Skip updating config.yml"
 )
 @click.pass_obj
 def save(
     context: Context,
     interactive: bool,
     set_vars: Config,
-    unset_vars: List[str],
+    unset_vars: t.List[str],
     env_only: bool,
 ) -> None:
     config = tutor_config.load_minimal(context.root)
@@ -71,7 +143,7 @@ def printroot(context: Context) -> None:
 
 
 @click.command(help="Print a configuration value")
-@click.argument("key")
+@click.argument("key", type=ConfigKeyParamType())
 @click.pass_obj
 def printvalue(context: Context, key: str) -> None:
     config = tutor_config.load(context.root)
