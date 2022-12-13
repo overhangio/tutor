6e73973835bb9a0afeef87f28c1a2d5216ca5ae9
Régis Behmo <regis@behmo.com>
2022-12-13 10:22:47 +0100

    fix: authn login in dev by disabling enterprise integration
    In development, login via the authn mfe was broken because of explicit
    enterprise integration:
    
        requests.exceptions.ConnectionError: HTTPConnectionPool(host='localhost', port=18000): Max retries exceeded with url: /enterprise/api/v1/enterprise-learner/?username=regis (Caused by NewConnectionError('<urllib3.connection.HTTPConnection object at 0x7fd4c02b8a90>: Failed to establish a new connection: [Errno 111] Connection refused'))
    
    See: https://discuss.overhang.io/t/tutor-login-fail-in-new-version/3083

--- tutor/templates/apps/openedx/settings/lms/development.py
+++ tutor/templates/apps/openedx/settings/lms/development.py
@@ -24,7 +24,11 @@ SESSION_COOKIE_SAMESITE = "Lax"
 # CMS authentication
 IDA_LOGOUT_URI_LIST.append("http://{{ CMS_HOST }}:8001/logout/")
 
-FEATURES['ENABLE_COURSEWARE_MICROFRONTEND'] = False
+FEATURES["ENABLE_COURSEWARE_MICROFRONTEND"] = False
+
+# Disable enterprise integration
+FEATURES["ENABLE_ENTERPRISE_INTEGRATION"] = False
+SYSTEM_WIDE_ROLE_CLASSES.remove("enterprise.SystemWideEnterpriseUserRoleAssignment")
 
 LOGGING["loggers"]["oauth2_provider"] = {
     "handlers": ["console"],
