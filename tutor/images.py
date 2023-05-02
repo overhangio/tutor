17f66fb467fd47939819725cf3f368b7664c6edc
Régis Behmo <regis@behmo.com>
2023-05-02 10:03:26 +0200

    refactor: simplify image tag management

--- tutor/images.py
+++ tutor/images.py
@@ -1,10 +1,4 @@
 from tutor import fmt, hooks, utils
-from tutor.types import Config, get_typed
-
-
-def get_tag(config: Config, name: str) -> str:
-    key = "DOCKER_IMAGE_" + name.upper().replace("-", "_")
-    return get_typed(config, key, str)
 
 
 def build(path: str, tag: str, *args: str) -> None:
