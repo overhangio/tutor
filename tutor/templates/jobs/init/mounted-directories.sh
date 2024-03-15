2acef736c0f367afcc4c5b75bd552d9a2fefd8ec
DawoudSheraz <dawoud.sheraz@gmail.com>
2024-03-15 16:35:34 +0500

    chore: update checks

--- tutor/templates/jobs/init/mounted-directories.sh
+++ tutor/templates/jobs/init/mounted-directories.sh
@@ -1,20 +1,21 @@
 # The initialization job contains various re-install operations needed to be done
-# on mounted directories (edx-platform, /mnt/*xblock)
-# 1. /mnt/*xblock
-# Whenever xblocks are mounted, during the image build time, they are copied over to container and
-# installed. This results in egg_info generation for the mounted xblocks. However, the egg_info is not carried
+# on mounted directories (edx-platform, /mnt/*xblock, /mnt/<edx-ora, search, enterprise>)
+# 1. /mnt/*
+# Whenever xblocks or other installable packages are mounted, during the image build, they are copied over to container
+# and installed. This results in egg_info generation for the mounted directories. However, the egg_info is not carried
 # over to host. When the containers are launched, the host directories without egg_info are mounted on runtime
 # and disappear from pip list.
+#
 # 2. edx-platform
 # When a new local copy of edx-platform is bind-mounted, certain build
 # artifacts from the openedx image's edx-platform directory are lost.
 # We regenerate them here.
 
 
-for mounted_xblock in /mnt/*xblock/; do
-    if ! ls "$mounted_xblock"*.egg-info >/dev/null 2>&1; then
-        echo "Unable to locate egg-info in $mounted_xblock"
-        pip install -e $mounted_xblock
+for mounted_dir in /mnt/*; do
+    if [ -f $mounted_dir/setup.py ] && ! ls $mounted_dir/*.egg-info >/dev/null 2>&1 ; then
+        echo "Unable to locate egg-info in $mounted_dir"
+        pip install -e $mounted_dir
     fi
 done
 
