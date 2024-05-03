6f6714e29d2080d8fc2b581f377890588a561fb1
DawoudSheraz <dawoud.sheraz@gmail.com>
2024-05-03 11:57:29 +0500

    v18.0.0

--- tutor/__about__.py
+++ tutor/__about__.py
@@ -10,7 +10,7 @@ __version__ = "17.0.6"
 # the nightly branch.
 # The suffix is cleanly separated from the __version__ in this module to avoid
 # conflicts when merging branches.
-__version_suffix__ = "nightly"
+__version_suffix__ = ""
 
 # The app name will be used to define the name of the default tutor root and
 # plugin directory. To avoid conflicts between multiple locally-installed
