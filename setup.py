301ac4e50186c9b0ce4b3d7d1f76a5aac459ede1
Syed Muhammad Dawoud Sheraz Ali <dawoud.sheraz@gmail.com>
2024-10-29 12:23:20 +0500

    chore: remove Python 3.8 references

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     long_description_content_type="text/x-rst",
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=load_requirements("base.in"),
     extras_require={
         "dev": load_requirements("dev.txt"),
@@ -69,7 +69,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
