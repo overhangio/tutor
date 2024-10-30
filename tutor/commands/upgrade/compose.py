e2786afa58106a9a006ee5aa224cc806ed1bb917
Danyal Faheem <138459282+Danyal-Faheem@users.noreply.github.com>
2024-10-30 17:00:48 +0500

    fix: upgrade MySQL from 5.7 to 8.1 first and then to 8.4 (#1149)
    * fix: upgrade MySQL from 5.7 to 8.1 first and then to 8.4
    This is required when upgrading from Tutor v15 to v18 directly
    MySQL does not allow direct upgrades from v5.7 to v8.4
    
    * fix: run MySQL 8.1 as a separate container during upgrade from Olive to Redwood (#1140)
    We do this because MySQL 8.1 does not have the --mysql-native-password option
    We have this option turned on for backward compatibility

--- tutor/commands/upgrade/compose.py
+++ tutor/commands/upgrade/compose.py
@@ -4,8 +4,9 @@ import click
 
 from tutor import config as tutor_config
 from tutor import env as tutor_env
+from tutor import hooks
 from tutor import fmt
-from tutor.commands import compose
+from tutor.commands import compose, jobs
 from tutor.types import Config
 
 from . import common as common_upgrade
@@ -157,6 +158,63 @@ def upgrade_from_olive(context: click.Context, config: Config) -> None:
     )
     upgrade_mongodb(context, config, "4.2.17", "4.2")
     upgrade_mongodb(context, config, "4.4.22", "4.4")
+
+    intermediate_mysql_docker_image = common_upgrade.get_intermediate_mysql_upgrade(
+        config
+    )
+    if not intermediate_mysql_docker_image:
+        return
+
+    click.echo(fmt.title(f"Upgrading MySQL to {intermediate_mysql_docker_image}"))
+
+    # We start up a mysql-8.1 container to build data dictionary to preserve
+    # the upgrade order of 5.7 -> 8.1 -> 8.4
+    # Use the mysql-8.1 context so that we can clear these filters later on
+    with hooks.Contexts.app("mysql-8.1").enter():
+        hooks.Filters.ENV_PATCHES.add_items(
+            [
+                (
+                    "local-docker-compose-services",
+                    """
+mysql-8.1:
+  extends: mysql
+  image: docker.io/mysql:8.1.0
+  command: >
+    mysqld
+    --character-set-server=utf8mb3
+    --collation-server=utf8mb3_general_ci
+    --binlog-expire-logs-seconds=259200
+    """,
+                ),
+                (
+                    "local-docker-compose-jobs-services",
+                    """
+mysql-8.1-job:
+  image: docker.io/mysql:8.1.0
+  depends_on: {{ [("mysql-8.1", RUN_MYSQL)]|list_if }}
+        """,
+                ),
+            ]
+        )
+        hooks.Filters.CONFIG_DEFAULTS.add_item(("MYSQL_HOST", "mysql-8.1"))
+
+        hooks.Filters.CLI_DO_INIT_TASKS.add_item(
+            ("mysql-8.1", tutor_env.read_core_template_file("jobs", "init", "mysql.sh"))
+        )
+
+    tutor_env.save(context.obj.root, config)
+
+    # Run the init command to make sure MySQL is ready for connections
+    context.invoke(jobs.initialise, limit="mysql-8.1")
+    context.invoke(compose.stop, services=["mysql-8.1"])
+
+    # Clear the filters added for mysql-8.1 as we don't need them anymore
+    hooks.clear_all(context="app:mysql-8.1")
+
+    # Save environment and run init for mysql 8.4 to make sure MySQL is ready
+    tutor_env.save(context.obj.root, config)
+    context.invoke(jobs.initialise, limit="mysql")
+    context.invoke(compose.stop, services=["mysql"])
 
 
 def upgrade_from_quince(context: click.Context, config: Config) -> None:
