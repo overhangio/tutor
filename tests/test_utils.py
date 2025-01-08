2dc0a1b371bad5e579bd86bca573d0848f9ba844
Régis Behmo <regis@behmo.com>
2025-01-08 10:00:42 +0100

    fix: pylint checks
    Following the latest changes in pylint configuration, a number of
    warnings were being reported.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,5 @@
 import base64
 import os
-import subprocess
 import tempfile
 import unittest
 from io import StringIO
--- tutor/commands/jobs_utils.py
+++ tutor/commands/jobs_utils.py
@@ -19,7 +19,6 @@ def get_mysql_change_charset_query(
     Utilized in the `tutor local do convert-mysql-utf8mb4-charset` command
     """
     return f"""
-            
             DROP PROCEDURE IF EXISTS UpdateColumns;
             DELIMITER $$
 
@@ -32,62 +31,62 @@ def get_mysql_change_charset_query(
                 DECLARE _column_name VARCHAR(255);
                 DECLARE _column_type VARCHAR(255);
                 DECLARE _collation_name VARCHAR(255);
-                
+
                 # We explicitly upgrade the utf8mb3_general_ci collations to utf8mb4_unicode_ci
                 # The other collations are upgraded from utf8mb3_* to utf8mb4_*
                 # For any other collation, we leave it as it is
                 DECLARE columns_cur CURSOR FOR
-                SELECT 
+                SELECT
                     TABLE_NAME,
                     COLUMN_NAME,
                     COLUMN_TYPE,
-                    CASE 
+                    CASE
                         WHEN COLLATION_NAME LIKE CONCAT('{charset_to_upgrade_from}', '_general_ci') THEN 'utf8mb4_unicode_ci'
                         WHEN COLLATION_NAME LIKE CONCAT('{charset_to_upgrade_from}', '_%') THEN CONCAT('{charset}', SUBSTRING_INDEX(COLLATION_NAME, '{charset_to_upgrade_from}', -1))
-                        ELSE COLLATION_NAME 
-                    END AS COLLATION_NAME 
-                FROM 
+                        ELSE COLLATION_NAME
+                    END AS COLLATION_NAME
+                FROM
                     INFORMATION_SCHEMA.COLUMNS
-                WHERE 
+                WHERE
                     TABLE_SCHEMA = '{database}'
                     AND COLLATION_NAME IS NOT NULL {query_to_append};
                 DECLARE CONTINUE HANDLER FOR NOT FOUND SET done_columns_loop = TRUE;
                 OPEN columns_cur;
                 columns_loop: LOOP
                     FETCH columns_cur INTO _table_name, _column_name, _column_type, _collation_name;
-                    
+
                     IF done_columns_loop THEN
                     LEAVE columns_loop;
                     END IF;
-                    
+
                     # First, upgrade the default charset and collation of the table
                     If _table_name <> _table_name_copy THEN
                     select _table_name;
                     SET FOREIGN_KEY_CHECKS = 0;
-                    SET @stmt = CONCAT('ALTER TABLE `', _table_name, '` CONVERT TO CHARACTER SET {charset} COLLATE {collation};'); 
+                    SET @stmt = CONCAT('ALTER TABLE `', _table_name, '` CONVERT TO CHARACTER SET {charset} COLLATE {collation};');
                     PREPARE query FROM @stmt;
                     EXECUTE query;
-                    DEALLOCATE PREPARE query; 
+                    DEALLOCATE PREPARE query;
                     SET FOREIGN_KEY_CHECKS = 1;
                     SET _table_name_copy = _table_name;
                     END IF;
-                    
+
                     # Then, upgrade the default charset and collation of each column
                     # This sequence of table -> column is necessary to preserve column defaults
                     SET FOREIGN_KEY_CHECKS = 0;
-                    SET @statement = CONCAT('ALTER TABLE `', _table_name, '` MODIFY `', _column_name, '` ', _column_type,' CHARACTER SET {charset} COLLATE ', _collation_name, ';'); 
+                    SET @statement = CONCAT('ALTER TABLE `', _table_name, '` MODIFY `', _column_name, '` ', _column_type,' CHARACTER SET {charset} COLLATE ', _collation_name, ';');
                     PREPARE query FROM @statement;
                     EXECUTE query;
-                    DEALLOCATE PREPARE query; 
+                    DEALLOCATE PREPARE query;
                     SET FOREIGN_KEY_CHECKS = 1;
-                        
+
                 END LOOP;
                 CLOSE columns_cur;
 
             END$$
 
             DELIMITER ;
-            
+
             DROP PROCEDURE IF EXISTS UpdateTables;
             DELIMITER $$
 
@@ -109,23 +108,23 @@ def get_mysql_change_charset_query(
                     IF done THEN
                     LEAVE tables_loop;
                     END IF;
-                    
+
                     select table_name_;
 
                     SET FOREIGN_KEY_CHECKS = 0;
-                    SET @stmt = CONCAT('ALTER TABLE `', table_name_, '` CONVERT TO CHARACTER SET {charset} COLLATE {collation};'); 
+                    SET @stmt = CONCAT('ALTER TABLE `', table_name_, '` CONVERT TO CHARACTER SET {charset} COLLATE {collation};');
                     PREPARE query FROM @stmt;
                     EXECUTE query;
-                    DEALLOCATE PREPARE query; 
-                    
+                    DEALLOCATE PREPARE query;
+
                     SET FOREIGN_KEY_CHECKS = 1;
-                        
+
                 END LOOP;
                 CLOSE cur;
-                
+
             END$$
             DELIMITER ;
-            
+
             use {database};
             ALTER DATABASE {database} CHARACTER SET {charset} COLLATE {collation};
             CALL UpdateColumns();
